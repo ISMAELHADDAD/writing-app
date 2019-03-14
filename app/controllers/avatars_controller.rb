32814d59d63a6d847c27cb5173bf94336abea129
Ismael Haddad <isma.7h@gmail.com>
2019-03-14 11:14:42 +0100

    action assign of Avatrs controller implemented + tests passing

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,2 +1,40 @@
 class AvatarsController < ApplicationController
+  before_action :set_avatar, only: [:assign]
+  before_action :authenticate, only: [:assign]
+
+  def assign
+    #If user doesn't exists
+    if !User.where(id: assign_params[:user_id]).exists?
+      render :json => {message: "User doesn't exists"}, status: :not_found
+      return
+    end
+
+    #If user isn't participating in the discussion or current_user is not involved with the discussion
+    if !Participant.where(discussion_id: assign_params[:discussion_id], user_id: assign_params[:user_id]).exists? ||
+      !Participant.where(discussion_id: assign_params[:discussion_id], user_id: current_user.id).exists?
+      render :json => {message: "User isn't participating in the discussion"}, status: :forbidden
+      return
+    end
+
+    if @avatar.update(user_id: assign_params[:user_id])
+      render :json => {message: "Avatar with id="+@avatar.id.to_s+
+        " is assigned to user with id="+assign_params[:user_id].to_s }, status: :ok
+    else
+      render :json => {message: "Couldn't assign avatar"}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_avatar
+    if params[:avatar_id]
+      @avatar ||= Avatar.find(params[:avatar_id])
+    else
+      @avatar = Avatar.find(params[:id])
+    end
+  end
+
+  def assign_params
+    params.permit(:discussion_id, :user_id)
+  end
 end
--- test/controllers/avatars_controller_test.rb
+++ test/controllers/avatars_controller_test.rb
@@ -1,7 +1,60 @@
 require "test_helper"
 
 class AvatarsControllerTest < ActionDispatch::IntegrationTest
-  # def test_sanity
-  #   flunk "Need real tests"
-  # end
+
+  test "should get success message on assign" do
+    put discussion_avatar_assign_url(discussion_id: 1, avatar_id: 2),
+      params: {
+        user_id: 1
+      },
+      headers: { "Authorization" => "123456" }
+
+    assert_match /Avatar with id=2 is assigned to user with id=1/, JSON.parse(response.body)['message']
+    assert_response :success
+  end
+
+  test "should get error message on assign with non-existing avatar" do
+    put discussion_avatar_assign_url(discussion_id: 1, avatar_id: 100),
+      params: {
+        user_id: 1
+      },
+      headers: { "Authorization" => "123456" }
+
+    assert_match /Couldn't find Avatar with 'id'=/, JSON.parse(response.body)['message']
+    assert_response :not_found
+  end
+
+  test "should get error message on assign with non-existing user" do
+    put discussion_avatar_assign_url(discussion_id: 1, avatar_id: 2),
+      params: {
+        user_id: 100
+      },
+      headers: { "Authorization" => "123456" }
+
+    assert_match /User doesn't exists/, JSON.parse(response.body)['message']
+    assert_response :not_found
+  end
+
+  test "should get error message on assign with non-participating user" do
+    put discussion_avatar_assign_url(discussion_id: 1, avatar_id: 2),
+      params: {
+        user_id: 4
+      },
+      headers: { "Authorization" => "123456" }
+
+    assert_match /User isn't participating in the discussion/, JSON.parse(response.body)['message']
+    assert_response :forbidden
+  end
+
+  test "should get error message on assign with non-participating user access" do
+    put discussion_avatar_assign_url(discussion_id: 1, avatar_id: 2),
+      params: {
+        user_id: 1
+      },
+      headers: { "Authorization" => "333333" }
+
+    assert_match /User isn't participating in the discussion/, JSON.parse(response.body)['message']
+    assert_response :forbidden
+  end
+
 end
